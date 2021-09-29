65fa5679bb03545110fc8075a8d638c867b33a7d
Ruben <rubencnd96@gmail.com>
2021-09-29 12:54:41 +0200

    ejercicios 10 y 11 terminados

---
+++ ejercicio10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+        int valor;
+
+        System.out.println("Ingrese un valor: ");
+
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+        valor = inputValue.nextInt();
+
+        if (valor >= 0) {
+            System.out.println("El valor es positivo");
+        } else {
+            System.out.println("El valor es negativo");
+        }
+        inputValue.close();
+    }
+}
---
+++ ejercicio11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ejercicio11 {
+    public static void main(String[] args) {
+        int x;
+        int y;
+
+        System.out.println("Ingresa un número: ");
+        
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+        x = inputValue.nextInt();
+
+        System.out.println("Ingresa otro número: ");
+        y = inputValue.nextInt();
+
+        if (x < y) {
+            System.out.println(x + ", " + y);
+        } else {
+            System.out.println(y + ", " + x);
+        }
+        inputValue.close();
+    }
+}
