d2966c75f16794729153ad89170b05b932c01078
Ruben <rubencnd96@gmail.com>
2021-09-29 12:15:53 +0200

    ejercicio 7 terminado

--- ejercicio7.java
+++ ejercicio7.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class ejercicio7 {
     public static void main(String[] args) {
-        double precio
+        double precioReal;
+        double precioRebajado;
+        double descuento;
+
+        System.out.println("Introduce el precio rebajado");
+
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+        precioRebajado = inputValue.nextFloat();
+
+        System.out.println("Introduce el precio real");
+        precioReal = inputValue.nextFloat();
+
+        descuento = (precioRebajado - precioReal) / precioRebajado * 100;
+        System.out.println("El descuento es:" + descuento + "%");
     }
 }
