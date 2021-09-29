1ab58a44eb69b3db74484b9541eb1e6065d6c985
Ruben <rubencnd96@gmail.com>
2021-09-29 13:35:02 +0200

    descarte

--- ejemplo2.java
+++
@@ -1,8 +0,0 @@
-public class ejemplo2 {
-	public static void main (String [] args) {
-		int dividendo = 20;
-		int divisor = 6;
-		int sumarAlFinal = 3;
-		System.out.println((dividendo/divisor)+sumarAlFinal);
-	}
-}
--- ejemplo3.java
+++
@@ -1,8 +0,0 @@
-public class ejemplo3 {
-	public static void main (String [] args) {
-		int squareArea =5*5;
-		
-
-		System.out.println(squareArea);
-	}
-}
