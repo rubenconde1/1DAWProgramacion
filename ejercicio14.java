af8d93c7c6104ba52172b9fd919ecccaa850afae
Ruben <rubencnd96@gmail.com>
2021-09-30 12:57:46 +0200

    ejercicio 14 terminado

---
+++ ejercicio14.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ejercicio14 {
+    public static void main(String[] args) {
+        int x;
+
+        System.out.println("Ingrese su nota: ");
+
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+        x = inputValue.nextInt();
+
+        if (x < 3) {
+            System.out.println("Muy deficiente");
+        } else {
+            if (x < 5) {
+                System.out.println("Insuficiente");
+            } else {
+                if (x < 6) {
+                    System.out.println("Suficiente");
+                } else {
+                    if (x < 7) {
+                        System.out.println("Bien");
+                    } else {
+                        if (x < 9) {
+                            System.out.println("Notable");
+                        } else {
+                            System.out.println("Sobresaliente");
+                        }
+                    }
+                }
+            }
+        }
+            
+    }
+}
