18625781ee3890710dfb0d221e5a932d44767f49
Ruben <rubencnd96@gmail.com>
2021-10-01 08:26:03 +0200

    ejercicio 15 terminado y creado 16

---
+++ ejercicio15.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ejercicio15 {
+    public static void main (String argv[]) {
+        int grade;
+        Scanner inputValue;
+        System.out.println("Introduce un número del 1 al 10: ");
+
+        inputValue = new Scanner(System.in);
+        grade = inputValue.nextInt();
+
+        switch (grade) {
+            case 1: System.out.println("uno");
+                break;
+            case 2: System.out.println("dos");
+                break;
+            case 3: System.out.println("tres");
+                break;
+            case 4: System.out.println("cuatro");
+                break;
+            case 5: System.out.println("cinco");
+                break;
+            case 6: System.out.println("seis");
+                break;
+            case 7: System.out.println("siete");
+                break;
+            case 8: System.out.println("ocho");
+                break;
+            case 9: System.out.println("nueve");
+                break;
+            case 10: System.out.println("diez");
+                break;
+            default: System.out.println("ERROR");
+        }
+        inputValue.close();
+    }
+}
---
+++ ejercicio16.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio16 {
+    public static void main(String[] args) {
+        int x, y;
+
+        System.out.println("Introduce dos valores.");
+        System.out.println("Introduce el primer valor: ");
+
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+        x = inputValue.nextInt();
+
+        System.out.println("Introduce el segundo valor: ");
+        y = inputValue.nextInt();
+
+        
+
+    }
+}
