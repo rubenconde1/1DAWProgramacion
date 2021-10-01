79cb2634edfd511ad988876e9cff2e834fd6f636
Ruben <rubencnd96@gmail.com>
2021-10-01 10:15:16 +0200

    ARREGLAR

--- ejercicio16.java
+++ ejercicio16.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class ejercicio16 {
     public static void main(String[] args) {
         int x, y;
+        String op;
 
-        System.out.println("Introduce dos valores.");
+        System.out.println("Introduce dos valores");
         System.out.println("Introduce el primer valor: ");
 
         Scanner inputValue;
@@ -13,8 +14,18 @@ public class ejercicio16 {
 
         System.out.println("Introduce el segundo valor: ");
         y = inputValue.nextInt();
-
         
+        System.out.println("Escribe que operación quieres hacer: +, -, *, / ");
+        /*Scanner miScanner = new Scanner (System.in);
+        op = miScanner.next();
+        orden = inputValue.next(); */
+        op = inputValue.next();
 
+        if (op == "suma") {
+            System.out.println("La suma es: " + (x + y) );
+        } else {
+            System.out.println("nada");
+        }
+        inputValue.close();
     }
 }
