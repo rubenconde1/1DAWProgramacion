b07906798778570a41fb2e9a68ead3e618f45195
Ruben <rubencnd96@gmail.com>
2021-10-08 10:06:32 +0200

    Ejercicio 26 terminado

---
+++ Ejercicio26.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio26 {
+    public static void main(String[] args) {
+        int n;
+
+        System.out.println("Input number:");
+
+        Scanner inputValue = new Scanner (System.in);
+        n = inputValue.nextInt();
+
+        inputValue.close();
+        
+        for (int i = 1; i <= 10; i++)
+            System.out.println(n + " * " + i + " = " + n * i);
+    }
+}
