cf3c8ef2ae4c3f6f91d6175e42433d6c2bb7f494
Ruben <rubencnd96@gmail.com>
2021-10-13 08:23:20 +0200

    Ejercicio 27 terminado

--- Ejercicio27.java
+++ Ejercicio27.java
@@ -15,7 +15,8 @@ public class Ejercicio27 {
 
         for (int i = 1; i <= n; i++){
             for (int j = 1; j <= i; j++)
-                System.out.println(i);
+                System.out.print(j);
+                System.out.println("");
         }
     }
 }
