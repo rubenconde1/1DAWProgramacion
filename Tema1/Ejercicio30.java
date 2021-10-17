0563b387baf859981635cf25777116ddf04741cf
Rubéen <rubencnd96@gmail.com>
2021-10-17 19:05:18 +0200

    Ejercicio 30 en desarrollo

--- Tema1/Ejercicio30.java
+++ Tema1/Ejercicio30.java
@@ -1,7 +1,22 @@
 package Tema1;
 
+import java.util.Scanner;
+
 public class Ejercicio30 {
     public static void main(String[] args) {
-        
+        String palabra;
+        boolean palindromo;
+        Scanner inputValue = new Scanner (System.in);
+
+        System.out.println("Introduzca una palabra:");
+        palabra = palabra.toLowerCase();
+        inputValue.close();
+
+        for (int i = 0, j = palabra.length() - 1; i <= j; i++, j--) {
+            if (palabra.charAt(i) != palabra.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
