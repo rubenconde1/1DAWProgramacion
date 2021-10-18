8b0636f08145d40b6e7457b8fe8e6f3d2e39aa0b
Ruben <rubencnd96@gmail.com>
2021-10-18 09:45:26 +0200

    Ejercicio 31 terminado

---
+++ Tema1/Ejercicio31.java
@@ -0,0 +1,12 @@
+package Tema1;
+
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        int [] array = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+ 
+
+        for (int i = 0; i < 10; i++) {
+        System.out.println("Element at index " + i + ": " + array[i]);
+        }
+    }
+}
