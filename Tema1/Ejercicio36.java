593d0aa69ee7f383fd8e506dd8f072c5bb323151
Ruben <rubencnd96@gmail.com>
2021-11-03 09:09:08 +0100

    Ejercicio 36 terminado

--- Tema1/Ejercicio36.java
+++ Tema1/Ejercicio36.java
@@ -4,25 +4,22 @@ import java.util.Scanner;
 public class Ejercicio36 {
     public static void main(String[] args) {
         Scanner inputValue = new Scanner (System.in);
-        int i, max = 0, min = 0;
-        int [] numeros = new int [10];
+        int i,j=1;
+        double max = 0;
+        double [] numeros = new double [10];
 
         System.out.println("Introduce diez números:");
-        for (i = 1; i <= 10; i++) {
-            System.out.println("Ingrese número " + i + ":");
-            numeros [i] = inputValue.nextInt();
-            if (min != 0 && max != 0) {
-                if (numeros[i] > max) {
-                    max = numeros[i];
-                }
-                if (numeros[i] < min) {
-                    min = numeros[i];
-                }
-            } else {
-                min = numeros[i];
-                max = numeros[i];
-            }
+        for (i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese número " + j++ + ":");
+            numeros [i] = inputValue.nextDouble();
         }
+        max = numeros[0];
+        
+        for (i = 1; i < numeros.length; i++) {
+            if (numeros[i] > max)
+            max = numeros[i];
+        }
+        
         System.out.println("Número máximo: " + max);
     }
 }
