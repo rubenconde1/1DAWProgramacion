de61f7e7fb975aa1029b2d37bd7967d98589d19d
Ruben <rubencnd96@gmail.com>
2021-11-05 09:21:22 +0100

    E03 terminado

---
+++ Controles/Control02/E03.java
@@ -0,0 +1,24 @@
+package Controles.Control02;
+
+import java.util.Scanner;
+
+public class E03 {
+    public static void main(String[] args) {
+        Scanner inputValue = new Scanner(System.in);
+        double promedio = 0, nota1, nota2, nota3, nota4;
+
+        nota1 = inputValue.nextDouble();
+        nota2 = inputValue.nextDouble();
+        nota3 = inputValue.nextDouble();
+        nota4 = inputValue.nextDouble();
+
+        promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("El promedio es: " + promedio);
+
+
+
+        
+
+    }
+}
