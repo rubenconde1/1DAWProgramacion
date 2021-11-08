bb163026fb623d502ec07b07fd4c979a939ee9b1
Ruben <rubencnd96@gmail.com>
2021-11-08 09:22:18 +0100

    Ejercicio 42 terminado

---
+++ Ejercicio41.java
@@ -0,0 +1,5 @@
+public class Ejercicio41 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Ejercicio42.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio42 {
+    public static void main(String[] args) {
+        Scanner inputValue = new Scanner(System.in);
+        System.out.print("Enter number 1:");
+        int x, y, mult;
+
+        x = inputValue.nextInt();
+
+        System.out.print("Enter number 2:");
+        y = inputValue.nextInt();
+        inputValue.close();
+
+        mult = x;
+        for (int i = 1; i < y; i++) {
+            mult = mult + x;   
+        }
+        System.out.println(mult);
+    }
+}
