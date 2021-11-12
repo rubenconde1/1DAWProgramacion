6889a05ba6ee223f73b4b6b51472e117b7ce8585
Ruben <rubencnd96@gmail.com>
2021-11-12 09:40:12 +0100

    Ejercicio positivos terminado

--- Tema2/Ejercicios/Positivos.java
+++ Tema2/Ejercicios/Positivos.java
@@ -1,13 +1,19 @@
 package Tema2.Ejercicios;
 
+
 public class Positivos {
     public static int positivos (int[] array) {
-    
+        int positivos1 = 0;
+
         for (int i = 0; i < array.length; i++) {
-            
+            if (array[i] > 0) {
+            positivos1++;
+            }
         }
+        return positivos1;
     } 
     public static void main(String[] args) {
-        
+        int[] array = {3, 10, -6, 9, 100, -45, -83, 323};
+        System.out.println(positivos(array));
     }
 }
