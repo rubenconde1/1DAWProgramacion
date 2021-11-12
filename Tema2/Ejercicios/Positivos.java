773e6c3f834d2c341737ea474bce4d3895d9c4e3
Ruben <rubencnd96@gmail.com>
2021-11-12 09:02:09 +0100

    prueba

--- Tema2/Ejercicios/Positivos.java
+++ Tema2/Ejercicios/Positivos.java
@@ -3,8 +3,6 @@ package Tema2.Ejercicios;
 public class Positivos {
     public static int positivos (int[] array) {
     
-        
-
         for (int i = 0; i < array.length; i++) {
             
         }
