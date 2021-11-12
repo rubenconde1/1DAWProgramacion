00d65521a3fe550ec4895f160f8cf6c5f87926ee
Ruben <rubencnd96@gmail.com>
2021-11-12 10:30:50 +0100

    Cambios en el código de Positivos.java

--- Tema2/Ejercicios/Positivos.java
+++ Tema2/Ejercicios/Positivos.java
@@ -3,14 +3,14 @@ package Tema2.Ejercicios;
 
 public class Positivos {
     public static int positivos (int[] array) {
-        int positivos1 = 0;
+        int positivo = 0;
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0) {
-            positivos1++;
+            if (array[i] >= 0) {
+            positivo++;
             }
         }
-        return positivos1;
+        return positivo;
     } 
     public static void main(String[] args) {
         int[] array = {3, 10, -6, 9, 100, -45, -83, 323};
