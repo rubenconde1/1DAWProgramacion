2e556a6a2a30efd6345d10906891e5aaff7fdfeb
Ruben <rubencnd96@gmail.com>
2021-11-15 09:43:10 +0100

    Ejercicio Primo terminado

---
+++ Tema2/EjerciciosValue/Primo.java
@@ -0,0 +1,24 @@
+package Tema2.EjerciciosValue;
+
+public class Primo {
+    public static boolean esPrimo (int x) {
+    boolean resultado = false;
+    int contador = 0;
+    
+    for (int i = 2; i <= x; i++) {
+    if(x % i == 0) {
+        contador++;
+        }
+    }
+    if(contador <= 2) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+    return resultado;
+}
+    public static void main(String[] args) {
+        System.out.println(esPrimo(20));
+        System.out.println(esPrimo(5));
+    }
+}
