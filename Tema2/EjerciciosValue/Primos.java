01b257c93813804c1ed5bcab0167baf973c9c217
Ruben <rubencnd96@gmail.com>
2021-11-15 09:47:53 +0100

    correción Primos.java

--- Tema2/EjerciciosValue/Primos.java
+++ Tema2/EjerciciosValue/Primos.java
@@ -3,9 +3,8 @@ package Tema2.EjerciciosValue;
 public class Primos {
     public static void main(String[] args) {
         for (int i = 3; i <= 100; i++) {
-            boolean resultado = Primo.esPrimo(i);
 
-            if (resultado) {
+            if (Primo.esPrimo(i)) {
                 System.out.println(i);
             }
         }
