6d03985357a33add947b875cb4472fd4bd65b486
Ruben <rubencnd96@gmail.com>
2021-11-15 09:46:47 +0100

    Correción método esPrimo (Primo.java)

--- Tema2/EjerciciosValue/Primo.java
+++ Tema2/EjerciciosValue/Primo.java
@@ -3,7 +3,7 @@ package Tema2.EjerciciosValue;
 public class Primo {
     public static boolean esPrimo (int x) {
     boolean resultado = false;
-    int contador = 0;
+    int contador = 1;
     
     for (int i = 2; i <= x; i++) {
     if(x % i == 0) {
