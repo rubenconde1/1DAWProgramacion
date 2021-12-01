6ffae6907800bf64977cdef25e4540e55648ae25
Ruben <rubencnd96@gmail.com>
2021-12-01 08:48:30 +0100

    Comunes.java terminado

--- Tema3/Comunes.java
+++ Tema3/Comunes.java
@@ -1,16 +1,25 @@
 package Tema3;
 
+import java.util.Arrays;
+
 public class Comunes {
     public static void main(String[] args) {
         int[] arrayPrincipal = new int [10];
         int[] arraySecundaria = new int [10];
 
-        Utilidades.rellenaArray(arrayPrincipal, 0, 10);
-        Utilidades.rellenaArray(arraySecundaria, 0, 10);
+        Utilidades.rellenaArray(arrayPrincipal, -10, 10);
+        Utilidades.rellenaArray(arraySecundaria, -10, 10);
 
-    }
+        System.out.println(Arrays.toString(arrayPrincipal));
+        System.out.println(Arrays.toString(arraySecundaria));
 
-    public static String comunes(int[] arrayP, int[] arrayS) {
-        
+    
+        for (int i = 0; i < arrayPrincipal.length; i++) {
+            for (int j = 0; j < arraySecundaria.length; j++) {
+                if (arrayPrincipal[i] == arraySecundaria[j]) {
+                    System.out.println("Números repetidos: " + arrayPrincipal[i]);
+                }
+            }
+        }
     }
 }
