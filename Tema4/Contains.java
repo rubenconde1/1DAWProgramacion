01b0061c7c7ee6b50445fadc422d0e46d047187e
Ruben <rubencnd96@gmail.com>
2021-12-10 10:37:50 +0100

    Contains.java terminado

---
+++ Tema4/Contains.java
@@ -0,0 +1,11 @@
+package Tema4;
+
+public class Contains {
+    public static void main(String[] args) {
+        String Cadena1 = Utilidades.leerCadena("Introduce una cadena");
+        String Cadena2 = Utilidades.leerCadena("Introduce una cadena");
+
+        System.out.println(Cadena1.contains(Cadena2));
+
+    }
+}
