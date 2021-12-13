b3caff477930dd28161839da7b028f569710f332
Ruben <rubencnd96@gmail.com>
2021-12-13 08:22:51 +0100

    Substring.java terminado

---
+++ Tema4/Substring.java
@@ -0,0 +1,9 @@
+package Tema4;
+
+public class Substring {
+    public static void main(String[] args) {
+        String Cadena1 = Utilidades.leerCadena("Ingrese una cadena");
+
+        System.out.println(Cadena1.substring(3, 5));
+    }
+}
