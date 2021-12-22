0d88a1e2ee6171bbf7e25cc81ee856560b4e8926
Ruben <rubencnd96@gmail.com>
2021-12-22 10:29:33 +0100

    ChorriChat método saludo terminado

--- Tema4/ChorriChat.java
+++ Tema4/ChorriChat.java
@@ -7,19 +7,20 @@ public class ChorriChat {
         /*String[] saludo = {"hola", "buenos dias", "buenas"};
         String[] bucle = {"Mm, es un tema interesante", "No he entendido lo que dices", "No hablo tu idioma"};
         String[] despedida = {"Chao", "ta luego", "adios", "bye"};*/
-        Scanner inputValue = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         String usuario = "";
+
         do{
-            usuario = inputValue.nextLine();
+            usuario = teclado.nextLine();
             System.out.println(esSaludo(usuario));
         }
-        while ((usuario == "hola") || (usuario == "buenos dias") || (usuario == "buenas"));
+        while (!(usuario.contains("hola") || usuario.contains("buenos dias") || usuario.contains("buenas")));
     }
         
-    public static String esSaludo(String usuario) {
+    public static String esSaludo(String saludoUsuario) {
         String saludoChorri = "";
 
-        if (usuario.equals("hola") || usuario.equals("buenos dias") || usuario.equals("buenas")) {
+        if (saludoUsuario.contains("hola") || saludoUsuario.contains("buenos dias") || saludoUsuario.contains("buenas")) {
             saludoChorri = "Hola, soy Chorri, tu asistente";
         } else {
             saludoChorri = "Eres un maleducado";
