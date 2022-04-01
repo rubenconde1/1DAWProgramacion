6216cda8740700d6627ec23814f33084d563adfb
Ruben <rubencnd96@gmail.com>
2022-04-01 10:37:59 +0200

    Fichero1 metodo getParent y numeración listado

--- Tema6/Fichero1.java
+++ Tema6/Fichero1.java
@@ -2,12 +2,12 @@ package Tema6;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Fichero1 {
-    
+    static File f = new File(".");
+    //Listar la ruta.
     public static File[] listRoots() {
         
         File raiz[] = File.listRoots();
@@ -23,52 +23,72 @@ public class Fichero1 {
         return raiz;
     }
 
+    //Imprimir los archivos del directorio actual.
     public static void imprimirDirectorioActual() throws IOException{
-        ArrayList<String> ficheros = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        int numero = -2;
-
-        File f = new File(".");
         System.out.println("Directorio actual: " + f.getCanonicalPath()+"\n");
         System.out.println("Lista de ficheros y directorios del directorio: ");
-        System.out.println("--------------------------------------------------\n");
+        System.out.println("--------------------------------------------------\n");   
 
-        //ListFiles??
-        for (String e : f.list()) {
-            ficheros.add(e);
-            Collections.sort(ficheros);
-        }
-        //Imprimir el array con un fori.
-        for (int i = 0; i < array.length; i++) {
-            
-        }
-        System.out.println(ficheros);
+        //Listar ficheros
+        File[] ficheros = f.listFiles();
+        System.out.println("0.- Directorio Padre");
 
-        while (numero != -1) {
-            try {
-                System.out.println("Selecciona un número para continuar. Pulse -1 para salir.");
-
-                numero = Integer.parseInt(scanner.nextLine());
-                switch (numero) {
-                    case 0:
-                        //Ir al directorio padre.
-                        break;
-                    case -1:
-                        System.out.println("Se ha cerrado el programa.");
-                        break;
-                
-                    default:
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println("Se ha producido un error.");
-            }
-            scanner.close();
+        for (int i = 0; i < ficheros.length; i++) {
+            Arrays.sort(ficheros);
+            System.out.println(i+1 + ".- " + ficheros[i].getName());
         }
+
+        System.out.println("Introduce una opción (-1 para salir):\n"); 
     }
     
+    public static void getParent() throws IOException {
+            File ficheroActual = new File(f.getCanonicalPath());
+            f = ficheroActual.getParentFile();
+    } 
+
     public static void main(String[] args) throws IOException {
+        int numero = 0;
+        Scanner scanner = new Scanner(System.in);
         listRoots();
-        imprimirDirectorioActual();
+        
+        do {
+            imprimirDirectorioActual();
+            numero = Integer.parseInt(scanner.nextLine());
+            switch (numero) {
+                case 0:
+                    getParent();
+                    break;
+
+                case (!= 0):
+            
+                default:
+                    break;
+            }
+            
+        } while (numero != -1);
+        System.out.println("El programa se ha cerrado.");
+        scanner.close(); 
     }
+
 }
+
+// while (numero != -1) {
+//     try {
+//         System.out.println("Selecciona un número para continuar:");
+
+//         numero = Integer.parseInt(scanner.nextLine());
+//         switch (numero) {
+//             case 0:
+//                 //Ir al directorio padre.
+//                 break;
+//             case -1:
+//                 System.out.println("Se ha cerrado el programa.");
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//     } catch (Exception e) {
+//         System.out.println("Se ha producido un error.");
+//     }
+// }
