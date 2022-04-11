a7c27792c386b105fb9aad829618a3359462eb14
Ruben <rubencnd96@gmail.com>
2022-04-11 09:54:37 +0200

    Mezcla método main

--- Tema6/Mezcla/Mezcla.java
+++ Tema6/Mezcla/Mezcla.java
@@ -7,20 +7,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import Tema6.Ficheros.Fichero1;
+
 public class Mezcla {
     public static void main(String[] args) throws IOException {
         //Dirección entera: /home/INFORMATICA/alu10193045/Escriptori/Programación/
         //Ruta de los ficheros origen y de destino.
-        String rutaFichero1 = "./Tema6/Mezcla/origen1.txt";
-        String rutaFichero2 = ".Tema6/Mezcla/origen2.txt";
-        String rutaDestino = "./Tema6/Mezcla/destino.txt";
 
-        File fichero1 = new File(rutaFichero1);
-        System.out.println(fichero1.getName());
-        File fichero2 = new File(rutaFichero2);
-        File destino = new File(rutaDestino);
-        System.out.println(fichero2.getName());
-        System.out.println(destino.getName());
+        File fichero1 = new File("./Tema6/Mezcla/origen1.txt");
+        File fichero2 = new File(".Tema6/Mezcla/origen2.txt");
+        File destino = new File("./Tema6/Mezcla/destino.txt");
+        unificarFicheros(fichero1, fichero2, destino);
     }
 
     public static void unificarFicheros(File fichero1, File fichero2, File destino) throws IOException {
