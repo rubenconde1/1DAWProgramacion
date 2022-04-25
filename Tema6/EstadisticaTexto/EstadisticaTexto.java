819278a2dcf7727f69609ccce733cdf8d3331334
Ruben <rubencnd96@gmail.com>
2022-04-25 09:56:48 +0200

    Ejercicio EstadisticaTexto

---
+++ Tema6/EstadisticaTexto/EstadisticaTexto.java
@@ -0,0 +1,18 @@
+package Tema6.EstadisticaTexto;
+
+public class EstadisticaTexto {
+    int contadorPalabras;
+    int contadorLineas;
+    int contadorLetras;
+    double porcentajeVocales;
+    double porcentajeConsonantes;
+    double porcentajeEspacios;
+    double porcentajeTabuladores;
+    public static void main(String[] args) {
+
+    }
+    
+    public double encontrarVocales(){
+        
+    }
+}
