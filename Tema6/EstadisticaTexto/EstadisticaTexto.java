da0a234fb9de1404c2111013fbb782409a3ec07b
Ruben <rubencnd96@gmail.com>
2022-04-27 09:15:30 +0200

    versión antes de reiniciar el método

--- Tema6/EstadisticaTexto/EstadisticaTexto.java
+++ Tema6/EstadisticaTexto/EstadisticaTexto.java
@@ -1,5 +1,12 @@
 package Tema6.EstadisticaTexto;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstadisticaTexto {
     int contadorPalabras;
     int contadorLineas;
@@ -8,11 +15,38 @@ public class EstadisticaTexto {
     double porcentajeConsonantes;
     double porcentajeEspacios;
     double porcentajeTabuladores;
-    public static void main(String[] args) {
-
+    List linias = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException{
+        File fichero = new File("./Tema6/EstadisticaTexto/Texto.txt");
+        
     }
     
-    public double encontrarVocales(){
-        
+    public double encontrarVocales(File fichero) throws IOException{
+        int contadorVocales = 0;
+        String linia = "";
+        char[] vocales = {'a','e','i','o','u','A','E','I','O','U'};
+        char[] vocalesLinea = linia.toCharArray();
+        BufferedReader reader = new BufferedReader(new FileReader(fichero));
+
+        try {
+            if (reader != null) {
+                while ((linia = reader.readLine()) != null) {
+                    linias.add(linia);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < vocalesLinea.length; i++) {
+            for (int j = 0; j < vocales.length; j++) {
+                if (vocalesLinea[i] == vocales[j]) {
+                    contadorVocales++;
+                }
+            }
+        }
+
+        return contadorVocales;
     }
 }
