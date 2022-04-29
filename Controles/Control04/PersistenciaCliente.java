f0ca31d5860a55717b24b1506e939223f01f8792
Ruben <rubencnd96@gmail.com>
2022-04-29 10:19:02 +0200

    Clase persistencia creada, falta probar

---
+++ Controles/Control04/PersistenciaCliente.java
@@ -0,0 +1,36 @@
+package Controles.Control04;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class PersistenciaCliente {
+    File fichero = new File("./Controles/Control04/clientes.data");
+    private static ArrayList<Cliente> listadoClientes = Cliente.getDatosCliente();
+    private static ArrayList<String> listadoLeidoClientes = new ArrayList<String>();
+    
+    //Método de la clase write
+    public void writer(Cliente datosCliente) throws IOException{
+        PrintWriter writer = new PrintWriter(fichero);
+
+        for (Cliente clienteAEscribir : listadoClientes) {
+            writer.println(clienteAEscribir + ",");
+        }
+        writer.close();
+    }
+
+    public void reader(File fichero) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fichero));
+        String lineaCliente = reader.readLine();
+        
+        while (lineaCliente != null) {
+            listadoLeidoClientes.add(lineaCliente);
+            lineaCliente = reader.readLine();
+        }
+        reader.close();
+        System.out.println(listadoLeidoClientes);
+    }
+}
