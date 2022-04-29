ac2474b198ed5ea52c7b1e2283dc28cafbfe33d3
Ruben <rubencnd96@gmail.com>
2022-04-29 10:18:30 +0200

    Clase Cliente terminada

--- Controles/Control04/Cliente.java
+++ Controles/Control04/Cliente.java
@@ -1,15 +1,19 @@
 package Controles.Control04;
 
+import java.util.ArrayList;
+
 public class Cliente extends Persona{
     //Atributos clase Cliente
     private int id;
     private String email;
+    private static ArrayList<Cliente> datosCliente = new ArrayList<>();
 
     //Constructor clase Cliente
     public Cliente(int id, String nif, String nombre, String apellidos, String email) {
         super(nif, nombre, apellidos);
         this.id = id;
         this.email = email;
+        datosCliente.add(this);
     }
 
     //Getters y Setters
@@ -25,10 +29,14 @@ public class Cliente extends Persona{
         this.email = email;
     }
 
+    public static ArrayList<Cliente> getDatosCliente() {
+        return datosCliente;
+    }
+
     //Métodos de la clase
     @Override
     public String toString() {
-        return "Datos Cliente \n" + "ID: \t" + id +"\n" + "NIF: \t" + nif + "\n" + "Nombre: \t" + nombre + "\n" + "Apellidos: \t" + apellidos + "\n" + "Email: \t" + email;
+        return "Datos Cliente \n" + "ID: \t" + id +"\n" + "NIF: \t" + nif + "\n" + "Nombre: \t" + nombre + "\n" + "Apellidos: \t" + apellidos + "\n" + "Email: \t" + email + ".";
     }
 
     
